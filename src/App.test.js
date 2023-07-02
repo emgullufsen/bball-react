765d914f739d6b7807232dd0c5f7331c49dc465e
Eric Gullufsen <ericgullufsen@gmail.com>
2023-07-02 19:39:59 -0400

    test uses scorescomponent now

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { ScoresComponent } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render( <ScoresComponent />, div);
 });
