b689d44e1a4f46dcfd8643cc1dc8b7b38888691d
Eric Gullufsen <ericgullufsen@gmail.com>
2026-05-24 19:49:21 -0400

    test use new render api

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { ScoresComponent } from './App';
+import { createRoot } from 'react-dom/client';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render( <ScoresComponent />, div);
+  const root = createRoot(div);
+  root.render(<ScoresComponent />);
 });
